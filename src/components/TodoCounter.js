67d992e5facc67030c9d03f6ef106c17781de07f
DESKTOP-3L63GNR\Liye- <liyeira@mimegafono.com>
2023-02-07 14:17:20 -0500

    TodoCounter: completed progress calculation fixed

--- src/components/TodoCounter.js
+++ src/components/TodoCounter.js
@@ -4,10 +4,13 @@ import { TodoContext } from "../context/TodoContext"
 
 function TodoCounter() {
     const {completedTodos, totalTodos} = React.useContext(TodoContext);
-    const completedPercentage = Math.round(100 * completedTodos / totalTodos);
+    let completedPercentage = 0;
+    if (totalTodos > 0) {
+        completedPercentage = Math.round(100 * completedTodos / totalTodos);
+    }
 
     return (
-        <div className="TodoCounter">
+        <div className={`TodoCounter ${totalTodos > 0 &&'TodoCounter--shown'}`}>
             <div className="TodoCounter__container">
                 <div className="TodoCounter__progress" style={{background: `conic-gradient(#abedd8 ${completedPercentage * 3.6}deg, transparent 0deg)`}}>
                     <span className="TodoCounter__value">{completedPercentage}%</span>
