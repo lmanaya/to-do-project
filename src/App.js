ba16038f4e1984f119d1cf07f52cfe05c781f2aa
DESKTOP-3L63GNR\Liye- <liyeira@mimegafono.com>
2023-02-06 20:51:40 -0500

    Use of useState

--- src/App.js
+++ src/App.js
@@ -7,28 +7,71 @@ import { TodoList } from "./components/TodoList";
 import { TodoItem } from "./components/TodoItem";
 import { CreateTodoButton } from "./components/CreateTodoButton";
 
-const todos = [
+const defaultTodos = [
   { text: 'Lavar la loza', complete: false },
   { text: 'Tomar agua', complete: false },
   { text: 'Pasear al perro', complete: true }
 ]
 
 function App() {
+  const [todos, setTodos] = React.useState(defaultTodos);
+  const [searchValue, setSearchValue] = React.useState('');
+
+  const completedTodos = todos.filter(todo => todo.complete).length;
+  const totalTodos = todos.length;
+
+  let renderedTodos = [];
+  if (searchValue.length === 0) {
+    renderedTodos = todos;
+  } else {
+    const searchText = searchValue.toLowerCase();
+    renderedTodos = todos.filter(todo => {
+      const todoText = todo.text.toLowerCase();
+      return todoText.includes(searchText);
+    });
+  }
+
+  const completeTodo = (text) =>{
+    const todoIndex = todos.findIndex(todo => todo.text === text);
+    const newTodos = [...todos];
+    newTodos[todoIndex].complete = !newTodos[todoIndex].complete;
+    setTodos(newTodos);
+  }
+
+  const deleteTodo = (text) =>{
+    const todoIndex = todos.findIndex(todo => todo.text === text);
+    const newTodos = [...todos];
+    newTodos.splice(todoIndex, 1)
+    setTodos(newTodos);
+  }
+
   return (
     <div className='TodoApp'>
       <div className='Todo'>
         <div className='Todo__header Todo--mb'>
           <div className='Todo__column'>
             <h1 className='Todo__title Todo--mb'>Mis tareas</h1>
-            <TodoSearch />
+            <TodoSearch
+              searchValue={searchValue}
+              setSearchValue={setSearchValue}
+            />
           </div>
           <div className='Todo__column'>
-            <TodoCounter />
+            <TodoCounter
+              completedTodos={completedTodos}
+              totalTodos={totalTodos}
+            />
           </div>
         </div>
         <TodoList>
-          {todos.map(todo => (
-            <TodoItem key={todo.text} text={todo.text} complete={todo.complete}/>
+          {renderedTodos.map(todo => (
+            <TodoItem
+              key={todo.text}
+              text={todo.text}
+              complete={todo.complete}
+              onComplete={() => completeTodo(todo.text)}
+              onDelete={() => deleteTodo(todo.text)}
+            />
           ))}
         </TodoList>
         <CreateTodoButton />
--- src/components/TodoCounter.js
+++ src/components/TodoCounter.js
@@ -1,12 +1,14 @@
 import React from "react";
 import './TodoCounter.css';
 
-function TodoCounter() {
+function TodoCounter(props) {
+    const completedPercentage = Math.round(100 * props.completedTodos / props.totalTodos);
+
     return (
         <div className="TodoCounter">
             <div className="TodoCounter__container">
-                <div className="TodoCounter__progress">
-                    <span className="TodoCounter__value">0%</span>
+                <div className="TodoCounter__progress" style={{background: `conic-gradient(#abedd8 ${completedPercentage * 3.6}deg, transparent 0deg)`}}>
+                    <span className="TodoCounter__value">{completedPercentage}%</span>
                 </div>
                 <span className="TodoCounter__text">Tareas <br />completadas</span>
             </div>
